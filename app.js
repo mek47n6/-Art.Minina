27f97bf444d1013f8dc966e12ceca5e17a8fe01b
mek47n6 <mek47n6@yandex.ru>
2024-01-16 12:10:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+    
+    // var img = document.getElementById("zoomM");
+
+    // img.onmouseover = function() {
+    //     this.style.transform = "scale(1.2)";
+    // }
+    // img.onmouseout = function() {
+    //     this.style.transform = "scale(1)";
+    // }
+
+function zoomIn (element) {
+    element.style.transform = "scale(1.3)"
+}
+function zoomOut (element) {
+    element.style.transform = "scale(1)"
+}
