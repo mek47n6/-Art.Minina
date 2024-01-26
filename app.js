fc926842d699dd9b97b03c753190b0c17635d1bf
mek47n6 <mek47n6@yandex.ru>
2024-01-26 13:50:11 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-// для модального окна
+
+    
+    // var img = document.getElementById("zoomM");
+
+    // img.onmouseover = function() {
+    //     this.style.transform = "scale(1.2)";
+    // }
+    // img.onmouseout = function() {
+    //     this.style.transform = "scale(1)";
+    // }
+
+// function zoomIn (element) {
+//     element.style.transform = "scale(1.3) translate(15%, 10%)";
+// }
+// function zoomOut (element) {
+//     element.style.transform = "scale(1) translate(0,0)";
+// }
+
 function openModal (imageSrc, description) {
     var modal = document.getElementById('myModal');
     var modalImg = document.getElementById('modalImg');
@@ -13,7 +30,7 @@ function closeModal(){
     modal.style.display = "none";
 }
 
-// для кнопки сворачивания текста
+
 document.getElementById('expandBtn').addEventListener('click', function() {
     var description = document.getElementsByClassName('description');
     description[0].classList.toggle('open');
@@ -22,5 +39,3 @@ document.getElementById('expandBtn').addEventListener('click', function() {
             this.textContent = 'Развернуть';
         }
 })
-
-
